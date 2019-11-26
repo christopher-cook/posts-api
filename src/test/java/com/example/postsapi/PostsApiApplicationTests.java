13e71f15ce26c61babebe3d55bc35d10fe333c59
Christopher Cook <cgcook81@gmail.com>
2019-11-26 14:38:19 -0500

    updated

--- src/test/java/com/example/postsapi/PostsApiApplicationTests.java
+++ src/test/java/com/example/postsapi/PostsApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.postsapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class PostsApiApplicationTests {
 
 	@Test
